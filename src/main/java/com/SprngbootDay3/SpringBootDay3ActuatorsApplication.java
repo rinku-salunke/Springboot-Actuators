8cba2c5a90d3c80f84172230284179c319bdc268
DESKTOP-3DOP2EC\ADMIN <rinkusalunke9@gmail.com>
2025-04-02 23:08:02 +0530

    1st commit

---
+++ src/main/java/com/SprngbootDay3/SpringBootDay3ActuatorsApplication.java
@@ -0,0 +1,13 @@
+package com.SprngbootDay3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDay3ActuatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDay3ActuatorsApplication.class, args);
+	}
+
+}
